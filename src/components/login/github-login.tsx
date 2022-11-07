0c19b91dc4272b037c9c6925818df94eec17114f
Yuuuk94 <yuuuk94@gmail.com>
2022-11-07 19:51:26 +0900

    style login

--- src/components/login/github-login.tsx
+++ src/components/login/github-login.tsx
@@ -1,6 +1,6 @@
 const GitHubLoginBtn = () => {
   return (
-    <button className="px-6 py-4 bg-black text-white text-md text-center w-full focus:outline-none rounded-full shadow-lg">
+    <button className="px-6 py-4 bg-darkGray text-lightGray text-md text-center w-full focus:outline-none rounded-full shadow-lg">
       Login with Git Hub
     </button>
   );
