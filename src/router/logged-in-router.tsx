6e7c8e95a273da13a5d36d4fbbebfd694fb01b0d
Yuuuk94 <yuuuk94@gmail.com>
2022-11-09 11:49:48 +0900

    fix router

--- src/router/logged-in-router.tsx
+++ src/router/logged-in-router.tsx
@@ -3,7 +3,7 @@ import { Logout } from " pages/logout-page";
 import Main from " pages/main-dashboard";
 import Header from "components/header/header-appbar";
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 const clientRoutes = [
   {
@@ -12,23 +12,19 @@ const clientRoutes = [
   },
 ];
 
-export const LoggedInRouter = () => {
-  return (
-    <>
-      <Header />
-      <Routes>
-        {clientRoutes.map((route) => {
-          return (
-            <Route
-              path={route.path}
-              key={route.path}
-              element={route.component}
-            />
-          );
-        })}
-        <Route path="/logout" element={<Logout />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
-    </>
-  );
-};
+export const LoggedInRouter = () => (
+  <>
+    <Header />
+    <Routes>
+      {clientRoutes.map((route) => {
+        return (
+          <Route path={route.path} key={route.path} element={route.component} />
+        );
+      })}
+      <Route path="/logout" element={<Logout />}></Route>
+      <Route path="/login" element={<Navigate to="/" replace={true} />} />
+      <Route path="/join" element={<Navigate to="/" replace={true} />} />
+      <Route path="*" element={<NotFound />}></Route>
+    </Routes>
+  </>
+);
