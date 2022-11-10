db9c988d7741358ce0d2be1015bf3ae3f16ae759
thewoowon <thewoowon@naver.com>
2022-11-10 13:28:04 +0900

    feat : complete kakao login

--- .history/src/components/login/kakao-login_20221109163501.tsx
+++ .history/src/components/login/kakao-login_20221109163501.tsx
@@ -3,12 +3,10 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 
 const KakaoLoginBtn = () => {
-  let kakaoParam = {
-    client_id : "b3a9beab04e6e23fce4144d6733c69ab",
-    redirect_uri : "http://127.0.0.1:3000/social/kakao",
-    response_type : "code"
-  }
-  const kakaoCodeURL = `https://kauth.kakao.com/oauth/authorize?${new URLSearchParams(kakaoParam).toString()}`;
+  const param = useParams();
+  const REST_API_KEY = "b3a9beab04e6e23fce4144d6733c69ab";
+  const REDIRECT_URI = `http://127.0.0.1:3000/social/kakao`;
+  const kakaoCodeURL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
 
   return (
     <button className="mb-4 px-6 py-4 bg-kakaoYello text-md text-center w-full focus:outline-none rounded-full shadow-lg">
