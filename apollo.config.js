fc7f91b006b6d5afb1c3cc32ee86db7eeec8a192
Yuuuk94 <yuuuk94@gmail.com>
2022-11-28 16:25:20 +0900

    style

--- apollo.config.js
+++ apollo.config.js
@@ -4,7 +4,7 @@ module.exports = {
     tagName: 'gql',
     service: {
       name: 'hi-agile',
-      url: 'http://localhost:4000/graphql',
+      url: 'http://hi-agile.herokuapp.com/graphql',
     },
   },
 };
